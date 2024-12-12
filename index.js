0caa8cbd2d58fc1fe43b05c5418e308158d3c7ef
Elijah <iamngunjiri@gmail.com>
2024-12-12 16:43:08 +0300

    Complete array find lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(arr) {
+    const win = arr.find(item => item.result === 'W'); // Find the first 'W' result
+    return win ? win.year : undefined; // Return the year or undefined if no 'W' is found
+}
